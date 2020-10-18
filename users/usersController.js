449675565166af57b4fc885768e6c9f3ccca9325
YuriiHaidai <ura.kiev@gmail.com>
2020-10-18 22:44:13 +0300

    fix

--- users/usersController.js
+++ users/usersController.js
@@ -99,7 +99,7 @@ class UsersController {
   }
 
   async getCurrent(req, res, next) {
-    const [user] = [req.user];
+    const user = req.user;
     return res
       .status(200)
       .json({ email: user.email, subscription: user.subscription });
